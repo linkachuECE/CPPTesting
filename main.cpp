cc48d28699faa87735f0bec34a93d5418fb7f941
Ethan <ethangbraun@gmail.com>
2021-08-05 15:32:37 -0500

    Intro to c++ classes. Getting interesting

--- main.cpp
+++ main.cpp
@@ -1,112 +1,25 @@
 #include "headers.h"
 #include <string>
 
-using ArithmeticFunction = int(*)(int, int);
+class Ball {
+    double m_radius{};
+    std::string m_color{};
 
-namespace arithmetic{
-
-    int add(int num1, int num2){
-        return num1 + num2;
+public:
+    Ball(double radius = 10.0, std::string color = "black"){
+        m_radius = radius;
+        m_color = color;
     }
 
-    int subtract(int num1, int num2){
-        return num1 - num2;
+    void print(){
+        std::cout << "Radius: " << m_radius << "\tColor: " << m_color << std::endl;
     }
 
-    int multiply(int num1, int num2){
-        return num1 * num2;
-    }
+};
 
-    int divide(int num1, int num2){
-        return num1 / num2;
-    }
+int main(){
+    Ball ball{5.0, "White"};
 
-    int modulo(int num1, int num2){
-        return num1 % num2;
-    }
-}
-
-ArithmeticFunction getArithmeticFunction(char op){
-    switch(op){
-        case '+':
-            return arithmetic::add;
-        case '-':
-            return arithmetic::subtract;
-        case '*':
-            return arithmetic::multiply;
-        case '/':
-            return arithmetic::divide;
-        case '%':
-            return arithmetic::modulo;
-    }
-}
-
-int getOperand(){
-    int num{};
-    while(true){
-        std::string input{};
-        std::cout << "Enter a number: ";
-        std::getline(std::cin, input);
-        std::cin.sync();
-
-        try{
-            num = std::stoi(input);
-            break;
-        } catch (std::invalid_argument){
-            std::cout << "Invalid input" << std::endl;
-        }
-    }
-
-    return num;
-}
-
-inline bool isOperator(char op){
-    switch(op){
-        case '+':
-        case '-':
-        case '/':
-        case '*':
-        case '%':
-            return true;
-        default:
-            return false;
-    }
-}
-
-int getOperator(){
-    char o{};
-    std::string input{};
-    while(true){
-        std::string input{};
-        std::cout << "Enter an operator (+, -, /, *, or %): ";
-        std::getline(std::cin, input);
-        std::cin.sync();
-
-
-        try{
-            o = input.at(0);
-            if (!isOperator(o))
-                throw std::invalid_argument("");
-            break;
-        } catch (std::invalid_argument){
-            std::cout << "Invalid input" << std::endl;
-        }
-    }
-
-    return o;
-}
-
-int main (){
-    int num1{}, num2{};
-    char op{};
-	std::cout << "Welcome to the calculator: " << std::endl;
-    num1 = getOperand();
-    num2 = getOperand();
-    op = getOperator();
-
-    ArithmeticFunction currentFunction{getArithmeticFunction(op)};
-
-    std::cout << "Your result is: " << currentFunction(num1, num2) << std::endl;
-
+    ball.print();
     return 0;
 }
