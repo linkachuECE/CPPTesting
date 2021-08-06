412a3f81cb09250d770bb6bd27b38a908e245791
Ethan <ethangbraun@gmail.com>
2021-08-06 13:24:45 -0500

    hurry

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include "headers.h"
 using namespace std;
 
-template <typename T, typename U>
-auto sub(T a, U b){
-    return a - b;
-}
+namespace animals {
+	enum animals{
+		chicken,
+		duck,
+		cat,
+		elephant,
+		dog,
+		snake,
+		animalMaxLength
+	};
+}  
 
-/*
-auto sub(auto a, auto b){
-    return a - b;
-}*/
 
 int main()
 {
-	std::cout << sub(3, 2) << '\n';
-	std::cout << sub(3.5, 2) << '\n';
-	std::cout << sub(4, 1.5) << '\n';
- 
+	int animalLegCount[animals::animalMaxLength]{};
+
+	animalLegCount[animals::chicken] = 2;
+	animalLegCount[animals::duck] = 2;
+	animalLegCount[animals::cat] = 4;
+	animalLegCount[animals::elephant] = 4;
+	animalLegCount[animals::dog] = 4;
+	animalLegCount[animals::snake] = 0;
+
 	return 0;
 }
