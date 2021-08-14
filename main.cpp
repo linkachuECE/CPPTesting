2fa59f68167c05c4123212c0c33b02747a2404e0
Ethan <ethangbraun@gmail.com>
2021-08-14 15:00:51 -0500

    Got through a couple chapters on class templates and virtual functions. Moving on to exceptions and handling soon. Nearly finished

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include "headers.h"
 
-int main(){
+#include <cassert>
+ 
+template <class T>
+class Pair1{
+private:
+    T m_value1{}, m_value2{};
 
-    return 0;
+public:
+    Pair1(T value1, T value2): m_value1{value1}, m_value2{value2}{}
+
+    T first() const{ return m_value1; }
+    T second() const{ return m_value2; }
+
+};
+
+template <class T, class U>
+class Pair{
+private:
+    T m_value1{};
+    U m_value2{};
+
+public:
+    Pair(T value1, U value2): m_value1{value1}, m_value2{value2}{}
+
+    T first() const{ return m_value1; }
+    U second() const{ return m_value2; }
+
+};
+
+
+int main()
+{
+	Pair<int, double> p1(5, 6.7);
+	std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+ 
+	const Pair<double, int> p2(2.3, 4);
+	std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+ 
+	return 0;
 }
