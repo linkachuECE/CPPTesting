59f66d7c3050fe981bb899693752356a0641a4f8
Ethan <ethangbraun@gmail.com>
2021-08-15 00:08:10 -0500

    whoops

--- main.cpp
+++ main.cpp
@@ -1,42 +1,36 @@
 #include "headers.h"
-
 #include <cassert>
- 
-template <class T>
-class Pair1{
+
+class Fraction{
 private:
-    T m_value1{}, m_value2{};
+    int m_numerator{};
+    int m_denominator{1};
 
 public:
-    Pair1(T value1, T value2): m_value1{value1}, m_value2{value2}{}
+    Fraction(int numerator, int denominator){
+        if (!denominator)
+            throw std::runtime_error("Denominator cannot be zero");
+        
+        m_denominator = denominator;
+        m_numerator = numerator;
+    }
 
-    T first() const{ return m_value1; }
-    T second() const{ return m_value2; }
+    ~Fraction(){}
 
-};
-
-template <class T, class U>
-class Pair{
-private:
-    T m_value1{};
-    U m_value2{};
-
-public:
-    Pair(T value1, U value2): m_value1{value1}, m_value2{value2}{}
-
-    T first() const{ return m_value1; }
-    U second() const{ return m_value2; }
-
+    friend std::ostream& operator<<(std::ostream& out, Fraction& fraction){
+        out << fraction.m_numerator << "/" << fraction.m_denominator;
+        return out;
+    }
 };
 
 
-int main()
-{
-	Pair<int, double> p1(5, 6.7);
-	std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
- 
-	const Pair<double, int> p2(2.3, 4);
-	std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
- 
+int main(){
+    try{
+        Fraction thisFraction{5, 2};
+        std::cout << thisFraction << std::endl;
+    } catch (std::runtime_error& error){
+        std::cout << error.what() << std::endl;
+    }
+
 	return 0;
 }
