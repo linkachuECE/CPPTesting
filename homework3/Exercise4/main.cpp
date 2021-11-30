5bfbfd940e68b8ab7d1eccf2723764f50d78c92c
Ethan Braun <ethangbraun@gmail.com>
2021-11-30 12:51:57 -0600

    Trying to get something to fucking work

--- homework3/Exercise4/main.cpp
+++ homework3/Exercise4/main.cpp
@@ -9,21 +9,26 @@
 #include <string>
 #include <iostream>
 
-bool sortedListNodeSearch(SortedListNode& root, const std::string searchWord){
+bool listNodeSearch(ListNode& root, const std::string searchWord){
     return root.search(searchWord);
 }
 
-bool listNodeSearch(ListNode& root, const std::string searchWord){
+bool sortedListNodeSearch(SortedListNode& root, const std::string searchWord){
     return root.search(searchWord);
-} 
-
-bool BSTSearch(BSTNode& root, const std::string searchWord){
-    return root.depthFirstSearch(searchWord);
 }
 
 bool BBTSearch(BBTNode& root, const std::string searchWord){
     return root.search(searchWord);
 }
+
+bool BSTSearch(BSTNode& root, const std::string searchWord){
+    return root.depthFirstSearch(searchWord);
+}
+
+void listNodeInsertion(ListNode& root, std::vector<std::string>* list){
+    for(auto i = list->begin() + 1; i < list->end(); i++)
+        root.addNode(*i);
+} 
 
 SortedListNode& sortedListNodeInsertion(SortedListNode& root, std::vector<std::string>* list){
     for(auto i = list->begin() + 1; i < list->end(); i++)
@@ -32,19 +37,14 @@ SortedListNode& sortedListNodeInsertion(SortedListNode& root, std::vector<std::s
     return root;
 }
 
-void listNodeInsertion(ListNode& root, std::vector<std::string>* list){
+void BBTInsertion(BBTNode& root, std::vector<std::string>* list){
     for(auto i = list->begin() + 1; i < list->end(); i++)
         root.addNode(*i);
-} 
+}
 
 void BSTInsertion(BSTNode& root, std::vector<std::string>* list){
     for(auto i = list->begin() + 1; i < list->end(); i++)
         root.wordInsert(*i);
-}
-
-void BBTInsertion(BBTNode& root, std::vector<std::string>* list){
-    for(auto i = list->begin() + 1; i < list->end(); i++)
-        root.addNode(*i);
 }
 
 std::vector<std::string>* fReader(const std::string filename){
@@ -54,27 +54,27 @@ std::vector<std::string>* fReader(const std::string filename){
     std::string currWord{};
     int num{};
 
-    reader >> num >> currWord;
+    reader >> currWord;
     std::vector<std::string>* list = new std::vector{currWord};
 
-    while(reader >> num >> currWord)
+    while(reader >> currWord)
         list->push_back(currWord);
 
     return list;
 }
 
 int main(){
-    std::vector<std::string>* list = fReader("test.txt");
+    std::vector<std::string>* list = fReader("key_file_50MB.txt");
 
-    BBTNode bbt{list->at(0)};
-    BSTNode bst{list->at(0)};
-    ListNode listNode{list->at(0)};
-    SortedListNode sortedListNode{list->at(0)};
+    ListNode lNode{list->at(0)};
 
-    BBTInsertion(bbt, list);
-    BSTInsertion(bst, list);
-    listNodeInsertion(listNode, list);
-    sortedListNode = sortedListNodeInsertion(sortedListNode, list);
+    time_t begin = time(NULL);
+
+    listNodeInsertion(lNode, list);
+
+    time_t end = time(NULL);
+
+    printf("Time elapsed is %d seconds\n", (end - begin));
 
     return 0;
 }
